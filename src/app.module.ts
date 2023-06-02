6db8036101258d6f6fa6561b235879a2c8f9d6d2
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-06-02 02:31:57 +0000

    Adding  post content

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+    MongooseModule.forRoot(process.env.DATA_BASE_URL, {
+      useNewUrlParser: true,
+    }),
+  ],
 })
 export class AppModule {}
