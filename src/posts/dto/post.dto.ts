7999b9a9f7b09c9675bbcead49ddb9eea17f7edc
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-06-02 23:03:57 +0000

    upating  post dto

--- src/posts/dto/post.dto.ts
+++ src/posts/dto/post.dto.ts
@@ -1,7 +1,23 @@
+import { IsString, IsNotEmpty, IsDateString } from 'class-validator';
+
 export class CreatePostDTO {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
   body: string;
+
+  @IsString()
+  @IsNotEmpty()
   author: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   date_posted: string;
 }
