ccb427ca08b244a1a94a61d6e5774e08b3d00419
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-06-23 03:18:32 +0000

    Moving everything back to express

--- src/shared/Interfaces/comment.interface.ts
+++
@@ -1,9 +0,0 @@
-//  comment interface for comment model
-
-import { Document } from 'mongoose';
-
-export interface Comment extends Document {
-  readonly comment: string;
-  readonly author: string;
-  readonly date_posted: string;
-}
--- src/shared/Interfaces/user.interface.ts
+++
@@ -1,7 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export interface User extends mongoose.Document {
-  readonly username: string;
-  readonly password: string;
-  readonly email: string;
-}
