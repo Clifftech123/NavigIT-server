2bba9034769fcb8833b11bba0a722645c583c936
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-08-11 20:46:47 +0000

    Adding of pot schema

---
+++ src/schemas/post.schema.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+import constantNumber from '../constants/number.constant'
+import IPost from '../interfaces/post.interface'
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: constantNumber.POST_TITLE_MIN_LENGTH,
+    maxlength: constantNumber.POST_TITLE_MAX_LENGTH,
+  },
+  content: {
+    type: String,
+    required: true,
+    minlength: constantNumber.POST_CONTENT_MIN_LENGTH,
+    maxlength: constantNumber.POST_CONTENT_MAX_LENGTH,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+PostSchema.set('toObject', { virtuals: true })
+PostSchema.set('toJSON', { virtuals: true })
+
+PostSchema.virtual('user', {
+  ref: 'User',
+  localField: 'author',
+  foreignField: '_id',
+  justOne: true,
+})
+
+export default mongoose.model<IPost>('Post', PostSchema)
