ffe6c335c7b0e9ae9589a06ee07babf979057455
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-08-12 12:55:24 +0000

    Adding of content of for the mode

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import CommentSchema from '../schemas/comment.schema'
 import CommentInterface from '../interfaces/comment.interface'
-import constantModel from '../constants//CommentModel.constant'
+import constantModel from '../constants/CommentModel.constant'
 const CommentModel = mongoose.model<CommentInterface>(
   constantModel.COMMENT_MODEL,
   CommentSchema,
