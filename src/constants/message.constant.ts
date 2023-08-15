784423936f91d77f6e4427b9d40fbe8963c25f1b
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-08-15 09:45:17 +0000

    Adding of user massage

--- src/constants/message.constant.ts
+++ src/constants/message.constant.ts
@@ -5,7 +5,7 @@ class message {
   //  Define a static property called API that is a string representing the path to the student API
   public static readonly SOMETHING_WENT_WRONG: string = 'Something went wrong'
 
-  // aut hmessage
+  // aut message
   public static readonly USERNAME_NOT_VALID: string = 'username is not valid'
   public static readonly NAME_NOT_VALID: string = 'name is not valid'
   public static readonly EMAIL_NOT_VALID: string = 'email is not valid'
@@ -28,6 +28,30 @@ class message {
   public static readonly NOT_AUTHENTICATED: string = 'Not authenticated'
   public static readonly UNAUTHORIZED: string = 'Unauthorized'
   public static readonly NOT_ALLOWED: string = 'Not allowed'
+
+  // user message
+
+  public static readonly USERNAME_NOT_CHANGE: string = 'username is not change'
+  public static readonly USERNAME_CHANGE_SUCCESS: string =
+    'username is change success'
+  public static readonly NAME_NOT_CHANGE: string = 'name is not change'
+  public static readonly NAME_CHANGE_SUCCESS: string = 'name is change success'
+  public static readonly EMAIL_NOT_CHANGE: string = 'email is not change'
+  public static readonly EMAIL_CHANGE_SUCCESS: string =
+    'email is change success'
+  public static readonly PASSWORD_NOT_CHANGE: string = 'password is not change'
+  public static readonly PASSWORD_CHANGE_SUCCESS: string =
+    'password is change success'
+  public static readonly PHONE_NOT_CHANGE: string = 'phone is not change'
+  public static readonly PHONE_CHANGE_SUCCESS: string =
+    'phone is change success'
+  public static readonly ADDRESS_NOT_CHANGE: string = 'address is not change'
+  public static readonly ADDRESS_CHANGE_SUCCESS: string =
+    'address is change success'
+  public static readonly USER_NOT_DELETE: string =
+    'user is not delete, please try again'
+  public static readonly USER_DELETE_SUCCESS: string = 'user is delete success'
+  public static readonly USER_FOUND: string = 'user is found'
 }
 
 export default message
