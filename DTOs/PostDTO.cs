e7af5c9eadccab9886dabbcd0a2f5c4915cb3b5c
Clifftech123 <opokuisaiahclifford123@gmail.com>
2023-10-03 00:52:36 +0000

    Dotnet initila commit

---
+++ DTOs/PostDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace student_connect_api.DTOs
+{
+    public class PostDTO
+    {
+        
+    }
+}
---
+++ Services/CommentService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace student_connect_api.Services
+{
+    public class CommentService
+    {
+        
+    }
+}
